e26cb91a56b8c13f77c6c6002cf7b1e506569dd8
Ruben Carrascal <krrskl97@gmail.com>
2025-08-05 22:55:54 -0500

    feat(startup): move to right folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:taskly/presentation/startup/presentation/startup_app.dart' show StartupApp;
 import 'package:taskly/presentation/startup/presentation/widgets/startup_widget.dart' show StartupWidget;
-import 'package:taskly/presentation/startup/startup_app.dart' show StartupApp;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
